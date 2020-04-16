f4d58b28cc6391c74aed3b7013c65087556e0e1f
Willyam Castro <willyam.castro@catolicasc.org.br>
2020-04-16 09:40:20 -0300

    add command running

--- Sistema Especialista para Classificação COVID19.pl
+++ Sistema Especialista para Classificação COVID19.pl
@@ -31,54 +31,70 @@
 
 % Fatos
 % paciente(nome, idade, temp, Freq. Cardiaca, Freq. respiratória, P_arterial, Sat_Oxigênio, Dispnéia, Comorbidades)
-paciente(larissa, 22, 36, 100, 80, 90, 95, true, 0).
-paciente(rafael, 21, 36, 150, 80, 90, 100, true, 2).
-paciente(willyam, 75, 39, 100, 25, 60, 150, false, 1).
-paciente(maia, 95, 37, 100, 80, 90, 100, false, 2).
+paciente(larissa, 22, 36, 100, 25, 90, 95, true, 0). % p_arterial e dispinéia grave (laranja)
+paciente(rafael, 21, 36, 150, 35, 110, 100, false, 0). % freq_respiratória gravissímo (vermelho)
+paciente(willyam, 65, 37.5, 105, 20, 120, 100, false, 1). % sem risco (amarelo)
+paciente(maia, 95, 37.2, 100, 25, 90, 100, false, 2). % idade e comorbidades graves (laranja)
+paciente(p_obs, 75, 39, 110, 30, 120, 100, false, 1). % sem risco (amarelo)
 
 
 % Regras
 
 % OR = ;
 % AND = ,
- 
-%Temperatura: Alto risco (laranja) = > que 39 °C
-febre_Alta(N):-paciente(N,_,T,_,_,_,_,_,_), T > 39.  
+
+%Idade: Baixo risco(amarelo) = 60 a 79 anos
+idade(N):-paciente(N,I,_,_,_,_,_,_,_), I >= 60, paciente(N,I,_,_,_,_,_,_,_), I =< 79.
+
+%Idade: Alto risco (laranja) = > 80 anos
+idade_alta(N):-paciente(N,I,_,_,_,_,_,_,_), I > 80.
+
 
 %Temperatura: Baixo risco(amarelo) = < que 35 ou entre 37 e 39 °C
 febre(N):-paciente(N,_,T,_,_,_,_,_,_), T < 35 ; paciente(N,_,T,_,_,_,_,_,_), T >= 37, paciente(N,_,T,_,_,_,_,_,_), T =< 39.
 
+%Temperatura: Alto risco (laranja) = > que 39 °C
+febre_alta(N):-paciente(N,_,T,_,_,_,_,_,_), T > 39.
+
+
 %Frequencia Cardiaca: Baixo risco(amarelo) = > 100 batimentos por minuto
 freq_cardiaca(N):-paciente(N,_,_,FC,_,_,_,_,_), FC > 100.
+
 
 %Frequencia respiratória: Baixo risco(amarelo) = Entre 19 a 30 ipm
 freq_respiratoria(N):-paciente(N,_,_,_,FR,_,_,_,_), FR >= 19, paciente(N,_,_,_,FR,_,_,_,_), FR =< 30.
 
 %Frequencia respiratória: Altissimo risco(vermelho) = > 30 ipm
-freq_respiratoria_Altissima(N):-paciente(N,_,_,_,FR,_,_,_,_), FR > 30.
+freq_respiratoria_altissima(N):-paciente(N,_,_,_,FR,_,_,_,_), FR > 30.
+
 
 %Pressão arterial: Alto risco (laranja) = > Entre 90 e 100 mmHg
-p_arterial_Alta(N):-paciente(N,_,_,_,_,PA,_,_,_), PA >= 90, paciente(N,_,_,_,_,PA,_,_,_), PA =< 100.
+p_arterial_alta(N):-paciente(N,_,_,_,_,PA,_,_,_), PA >= 90, paciente(N,_,_,_,_,PA,_,_,_), PA =< 100.
 
 %Pressão arterial: Altissimo risco(vermelho) = < 90 mmHg
-p_arterial_Altissima(N):-paciente(N,_,_,_,_,PA,_,_,_), PA < 90.
+p_arterial_altissima(N):-paciente(N,_,_,_,_,PA,_,_,_), PA < 90.
+
 
 %Saturação de Oxigênio: Altissimo risco(vermelho) = < 95%
-sat_oxigenio_Altissima(N):-paciente(N,_,_,_,_,_,SO,_,_), SO < 95.
+sat_oxigenio_altissima(N):-paciente(N,_,_,_,_,_,SO,_,_), SO < 95.
 
 %Dispnéia: Altissimo risco(vermelho) = True
 dispneia(N):-paciente(N,_,_,_,_,_,_,D,_), D = true.
 
-%Idade: Baixo risco(amarelo) = 60 a 79 anos 
-idade(N):-paciente(N,I,_,_,_,_,_,_,_), I >= 60, paciente(N,I,_,_,_,_,_,_,_), I =< 79.
-
-%Idade: Alto risco (laranja) = > 80 anos
-idade_Alta(N):-paciente(N,I,_,_,_,_,_,_,_), I > 80.
 
 %Comorbidades: Baixo risco(amarelo) = 1
 comorbidade(N):-paciente(N,_,_,_,_,_,_,_,C), C = 1.
 
 %Comorbidades: Alto risco (laranja) = 2 ou +
+comorbidade_alta(N):-paciente(N,_,_,_,_,_,_,_,C), C >= 2.
 
-comorbidade_Alta(N):-paciente(N,_,_,_,_,_,_,_,C), C >= 2.
+
+
+% Running...
+% 
+%  Paciente sem risco, mas deve ficar em observação:
+%	idade(X), febre(X), freq_cardiaca(X), freq_respiratoria(X), comorbidade(X)
+%	
+%  Paciente com risco, deve ser encaminhado para hospital:
+%	idade_alta(X); febre_alta(X); freq_respiratoria_altissima(X); p_arterial_altissima(X); sat_oxigenio_altissima(X); dispneia(X); comorbidade_alta(X).
 
